53d2445bd2edef4e454573d96415e9f29936a82a
numan <numanabid27@gmail.com>
2024-08-05 17:32:44 +0500

    video

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -8,7 +8,7 @@ import project from "@/assets/images/project.svg";
 import data from "@/assets/images/data.svg";
 import cost from "@/assets/images/cost.svg";
 import Image from "next/image";
-import practice from "@/assets/images/best-practice.png"
+import practice from "@/assets/images/best-practice.png";
 import ChooseUs from "../common/choose-us";
 import { Partners } from "../common/partners";
 import web from "@/assets/images/web.svg";
@@ -61,21 +61,27 @@ export function Home() {
       desc: "Designing captivating user interfaces and experiences that delight users.",
       img: ui,
     },
-  ]
+  ];
   return (
     <>
-      <Hero
-        img={banner}
-        tagLine="Welcome to Preach Concepts LLC"
-        title={
-          <>
-            Your Partner in <span style={{ color: "#005AAB" }}>SaaS</span>{" "}
-            Product Management and Software Development
-          </>
-        }
-        para="At Preach Concepts LLC, we turn innovative ideas into successful, scalable products, ensuring market fit and top-notch development for competitive success."
-        button="Start Your Project Today"
-      />
+      <section className={styles.video_sec}>
+        <video autoPlay muted loop playsInline>
+          <source
+            src="https://global.divhunt.com/1113f6c9b641b05eacb1d7e596f20553_7887992.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <div className="container">
+          <div className={styles.banner_desc}>
+            <h6>Welcome to Preach Concepts LLC</h6>
+            <h1>Your Partner in <span style={{ color: "#005AAB" }}> SaaS </span>
+             Product Management and Software Development</h1>
+            <p>At Preach Concepts LLC, we turn innovative ideas into successful, scalable products, ensuring market fit and top-notch development for competitive success.</p>
+            <button>Start Your Project Today</button>
+          </div>
+        </div>  
+      </section>
+
       <section className={styles.serivces_row + "" + " container"}>
         <h2>
           Product Management <span>Services</span>
@@ -96,7 +102,9 @@ export function Home() {
         <h2>
           Development <span>Services</span>
         </h2>
-        <p>We offer a range of development services to bring your vision to life:</p>
+        <p>
+          We offer a range of development services to bring your vision to life:
+        </p>
         <div className={styles.card_grid}>
           {development_Services.map((item, i) => {
             return (
@@ -112,8 +120,15 @@ export function Home() {
         <div className="container">
           <div className={styles.best_practice_Row}>
             <div className={styles.best_practice_col}>
-              <h2>Agile Best <span>Practices</span></h2>
-              <p>We help you implement Agile best practices, fostering a collaborative and efficient development environment. Our approach ensures quick iterations, continuous feedback, and rapid delivery of high-quality products.</p>
+              <h2>
+                Agile Best <span>Practices</span>
+              </h2>
+              <p>
+                We help you implement Agile best practices, fostering a
+                collaborative and efficient development environment. Our
+                approach ensures quick iterations, continuous feedback, and
+                rapid delivery of high-quality products.
+              </p>
             </div>
             <div className={styles.best_practice_col2}>
               <Image src={practice} alt="" />
